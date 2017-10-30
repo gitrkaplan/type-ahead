123891ced68f95bdc86f1a79b52bc104d7a00858
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-30 09:37:53 -0700

    Adds simple logic to complete the expected results of the typeahead. Also styles the li elements to have them match the rest of the layout. #1

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@
 
 const input = document.querySelector('#term')
 const matches = document.querySelector('#matches')
+const placeholder = document.querySelector('#placeholder')
 
 const words = () => {
   return fetch('/typeahead/words').then(res => res.json())
@@ -9,17 +10,21 @@ const words = () => {
 
 input.addEventListener('keyup', () => {
   let current = input.value.toLowerCase()
+  console.log(current)
   words().then(results => findString(current, results))
 })
 
 const findString = (str, array) => {
   clearList()
   if (str !== '') {
+    placeholder.classList.add('hidden')
     for (let i = 0; i < array.length; i++) {
       if (array[i].word.indexOf(str) > -1) {
         renderWord(array[i].word)
       }
     }
+  } else {
+    placeholder.classList.remove('hidden')
   }
 }
 
@@ -31,8 +36,7 @@ const renderWord = word => {
 }
 
 const clearList = () => {
-  const $list = document.getElementsByTagName('li')
-  for (var i = 0; i < $list.length; i++) {
-    matches.removeChild($list[i])
+  while (matches.hasChildNodes()) {
+    matches.removeChild(matches.lastChild)
   }
 }
